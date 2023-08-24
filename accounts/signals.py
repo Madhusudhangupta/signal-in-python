3d3318fb021ac1656d79202127129d373d7ba661
Madhusudhan <madhusudhangupta18@gmail.com>
2023-08-25 00:15:52 +0530

    modified files

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,5 +1,6 @@
 from django.dispatch import Signal, receiver
 from django.contrib.auth.models import User
+from models import Notification
 
 # define a signal
 user_registered = Signal()
@@ -11,5 +12,10 @@ def send_welcome_notification(sender, **kwargs):
     print(f"Sending welcome notification to {user.username}")
 
     # add notification logic here
+    notification = Notification.objects.create(
+        user = user,
+        message = f"Welcome, {user.username}! TThank you for joining us."
+    )
 
+    print(f"Sending welcome notification to {user.username}")
     
